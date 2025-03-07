437a1846800df4e56b6a02dbf2f4e691de3d14db
masatake aoki <aoki@rt-net.jp>
2025-03-07 13:18:46 +0900

    add STEP2

--- pico_classic_v4_STEP2_SWITCH/pico_classic_v4_STEP2_SWITCH.ino
+++ pico_classic_v4_STEP2_SWITCH/pico_classic_v4_STEP2_SWITCH.ino
@@ -16,72 +16,59 @@
 // メッセージヘッダーファイルを見つけるため、micro_ros_arduino.hを先にインクルードすること
 #include <micro_ros_arduino.h>
 #include <rcl/error_handling.h>
-#include <rclc/executor.h>
 #include <rcl/rcl.h>
 #include <rclc/rclc.h>
 #include <stdio.h>
 #include <std_msgs/msg/int32.h>
 // clang-format off
 
-#define PCC4
-
 std_msgs__msg__Int32 g_msg;
 
-rcl_subscription_t g_subscriber;
-rclc_executor_t g_executor;
+rcl_publisher_t g_publisher;
 rclc_support_t g_support;
 rcl_allocator_t g_allocator;
 rcl_node_t g_node;
+
+#define PCC4
 
 #ifdef PCC4
 #define LED0 13
 #define LED1 14
 #define LED2 47
 #define LED3 48
+#define SW_L 16
+#define SW_C 15
+#define SW_R 18
 #else
 #define LED0 1
 #define LED1 2
 #define LED2 42
 #define LED3 41
+#define SW_L 10
+#define SW_C 11
+#define SW_R 12
 #endif
 
-#define RCCHECK(fn)                \
-  {                                \
-    rcl_ret_t temp_rc = fn;        \
-    if ((temp_rc != RCL_RET_OK)) { \
-      errorLoop();                \
-    }                              \
-  }
-#define RCSOFTCHECK(fn)            \
-  {                                \
-    rcl_ret_t temp_rc = fn;        \
-    if ((temp_rc != RCL_RET_OK)) { \
-    }                              \
-  }
+#define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){error_loop();}}
+#define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){}}
 
-void errorLoop()
-{
-  while (1) {
+
+void error_loop(){
+  while(1){
     digitalWrite(LED0, !digitalRead(LED0));
     delay(100);
   }
 }
 
-void subscription_callback(const void * msgin)
-{  
-  const std_msgs__msg__Int32 * msg = (const std_msgs__msg__Int32 *)msgin;
-  digitalWrite(LED0, (((msg->data)&0x01) == 0) ? LOW : HIGH);  
-  digitalWrite(LED1, (((msg->data)&0x02) == 0) ? LOW : HIGH);  
-  digitalWrite(LED2, (((msg->data)&0x04) == 0) ? LOW : HIGH);  
-  digitalWrite(LED3, (((msg->data)&0x08) == 0) ? LOW : HIGH);  
-}
-
-void setup()
-{
+void setup() {
   pinMode(LED0, OUTPUT);
   pinMode(LED1, OUTPUT);
   pinMode(LED2, OUTPUT);
   pinMode(LED3, OUTPUT);
+
+  pinMode(SW_L, INPUT_PULLUP);
+  pinMode(SW_C, INPUT_PULLUP);
+  pinMode(SW_R, INPUT_PULLUP);
 
   digitalWrite(LED1, HIGH);
   set_microros_wifi_transports("使用するWiFiのAP名", "Wi-Fiのパスワード", "PCのIPアドレス", 8888);
@@ -97,19 +84,16 @@ void setup()
   // create g_node
   RCCHECK(rclc_node_init_default(&g_node, "micro_ros_pico_node", "", &g_support));
 
-  // create subscriber
-  RCCHECK(rclc_subscription_init_default(
-    &g_subscriber,
+  // create publisher
+  RCCHECK(rclc_publisher_init_best_effort(
+    &g_publisher,
     &g_node,
     ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
-    "micro_ros_arduino_subscriber"));
+    "topic_name"));
 
-  // create executor
-  RCCHECK(rclc_executor_init(&g_executor, &g_support.context, 1, &g_allocator));
-  RCCHECK(rclc_executor_add_subscription(&g_executor, &g_subscriber, &g_msg, &subscription_callback, ON_NEW_DATA));
 }
 
 void loop() {
-  delay(10);
-  RCCHECK(rclc_executor_spin_some(&g_executor, RCL_MS_TO_NS(100)));   
+    RCSOFTCHECK(rcl_publish(&g_publisher, &g_msg, NULL));
+    g_msg.data=digitalRead(SW_L)&digitalRead(SW_C)&digitalRead(SW_R);
 }
