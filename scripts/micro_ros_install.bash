720bcf09ea1cac92b311d1fa0acdad0e2c4167ff
maaoki <aoki@rt-net.jp>
2025-03-17 08:39:22 +0900

    scripts add

---
+++ scripts/micro_ros_install.bash
@@ -0,0 +1,14 @@
+!/bin/bash
+cd ~
+mkdir -p ~/uros_seminar_ws/src
+cd ~/uros_seminar_ws/src
+git clone -b humble https://github.com/micro-ROS/micro_ros_setup
+sudo apt update && rosdep update
+source /opt/ros/humble/setup.bash
+cd ~/uros_seminar_ws
+rosdep install --from-paths src --ignore-src -y
+colcon build
+ros2 run micro_ros_setup create_agent_ws.sh
+colcon build
+source install/local_setup.bash
+
